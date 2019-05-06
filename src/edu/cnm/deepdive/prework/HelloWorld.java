f0389927d19dd9ae9f8ee7cfc9bec701b36b1f28
Monjoie11 <jeff.franken@gmail.com>
2019-05-06 08:12:53 -0600

    commit with values and command argument changes. This shit works, dawg!

--- src/edu/cnm/deepdive/prework/HelloWorld.java
+++ src/edu/cnm/deepdive/prework/HelloWorld.java
@@ -3,7 +3,9 @@ package edu.cnm.deepdive.prework;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    System.out.println("Look, Ma, I can make my computer do stuff!") ;
+      salute( (args.length > 0) ? args[0] : "unfathomable nothingness" ) ;
   }
-
+ private static void salute(String target) {
+    System.out.println("Look," + target + ",I can make my computer do stuff!") ;
+ }
 }
