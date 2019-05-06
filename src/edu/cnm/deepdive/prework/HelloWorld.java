2fe86967a46a8b840266543cd1fcd37d749fef7a
Monjoie11 <jeff.franken@gmail.com>
2019-05-06 08:48:31 -0600

    add sultation method to construct salutation string

--- src/edu/cnm/deepdive/prework/HelloWorld.java
+++ src/edu/cnm/deepdive/prework/HelloWorld.java
@@ -3,9 +3,13 @@ package edu.cnm.deepdive.prework;
 public class HelloWorld {
 
   public static void main(String[] args) {
-      salute( (args.length > 0) ? args[0] : "unfathomable nothingness" ) ;
+      salute(salutation (args));
   }
- private static void salute(String target) {
-    System.out.println("Look," + target + ",I can make my computer do stuff!") ;
+  private static String salutation(String[] args){
+    return "Look," + ((args.length > 0) ? args[0] : "unfathomable nothingness" ) + ",I can make my computer do stuff!" ;
+  }
+
+ private static void salute(String salutation) {
+    System.out.println(salutation) ;
  }
 }
